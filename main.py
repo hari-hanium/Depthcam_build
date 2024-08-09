7795c4085618efd5bbbff486b30c01ac78b1e395
잉여개발기 <min08101@naver.com>
2024-08-09 14:49:36 +0900

    보정 코드 추가

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ profile = pipeline.start(config)
 align_to = rs.stream.color
 align = rs.align(align_to)
 
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+
+limit_min = 30
+limit_max = 120
+
 while 1:
     frames = pipeline.wait_for_frames()
     depth = frames.get_depth_frame()
@@ -46,8 +52,22 @@ while 1:
     width = color_image.shape[1]
     height = color_image.shape[0]
 
-    depth_image = cv2.convertScaleAbs(depth_image, alpha = 0.03)
-    depth_color = cv2.applyColorMap(depth_image, cv2.COLORMAP_JET)
+    depth_image = depth_image * depth_scale * 100
+    # print(scale_depth_image)
+
+    depth_mask = depth_image
+    depth_mask[depth_mask > limit_max] = limit_max
+    depth_mask[depth_mask < limit_min] = limit_min
+    # print(depth_mask)
+
+    min_val = np.min(depth_mask)
+    max_val = np.max(depth_mask)
+
+    normalized_image = 255 * (depth_mask - min_val) / (max_val - min_val)
+    normalized_image = normalized_image.astype(np.uint8)
+    # print(min_val, max_val, normalized_image)
+
+    depth_color = cv2.applyColorMap(normalized_image, cv2.COLORMAP_JET)
 
     frame_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
@@ -70,13 +90,20 @@ while 1:
                 result_process[ids] = [cx, cy]
 
                 cv2.circle(img, (cx, cy), 1, (255, 255, 0), -1, cv2.LINE_AA)
-                cv2.circle(depth_color, (cx, cy), 1, (255, 255, 0), -1, cv2.LINE_AA)
+                cv2.circle(color_image, (cx, cy), 1, (255, 255, 0), -1, cv2.LINE_AA)
 
-    if 8 in result_process:
+    if 8 in result_process and 7 in result_process:
         # print(result_process[8][1], result_process[8][0])
-        print(depth_image[result_process[8][1]][result_process[8][0]])
+        y = int((result_process[8][1] + result_process[7][1]) / 2)
+        x = int((result_process[8][0] + result_process[7][0]) / 2)
+
+        if depth_image[y][x] == limit_min:
+            pass
+        else:
+            print(depth_image[y][x])
 
     # images = cv2.addWeighted(img, 0.5, depth_color, (1 - 0.5), 0) 
+    # images = color_image
     images = depth_color
 
     cv2.imshow('Test', images)
